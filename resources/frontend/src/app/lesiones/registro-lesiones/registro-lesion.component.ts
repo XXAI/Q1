ed7d20702a4373849e53d133b43499268daff323
Mario Villatoro <ing.mario.villatoro@gmail.com>
2022-11-09 13:30:44 -0600

    actualizacion de causas

--- resources/frontend/src/app/lesiones/registro-lesiones/registro-lesion.component.ts
+++ resources/frontend/src/app/lesiones/registro-lesiones/registro-lesion.component.ts
@@ -361,11 +361,16 @@ export class RegistroLesionComponent implements OnInit {
                 a1['tipo_'+element.rel_causa_conductor_id] = 1;
               });
               let co1 = response.causa_conductor_detalle[0];
-              a1.sexo = co1.sexo_id;
-              a1.aliento_alcoholico= co1.alcoholico;
-              a1.cinturon_seguridad=co1.cinturon;
-              a1.edad=co1.edad;
-              this.tipoConductorForm.patchValue(a1);
+              
+              if(co1)
+              {
+                a1.sexo = co1.sexo_id;
+                a1.aliento_alcoholico= co1.alcoholico;
+                a1.cinturon_seguridad=co1.cinturon;
+                a1.edad=co1.edad;
+                this.tipoConductorForm.patchValue(a1);
+                
+              }
               break;
               case 2:
                 let pe = response.causa_peaton;
